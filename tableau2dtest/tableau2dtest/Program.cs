949d979da5073ad4b96805c58630bba0feff2e90
Vincent REY <v.rey@cite-scolaire.lan>
2023-10-16 18:03:19 +0200

    ajout tableau 3x3 16/10/2023

--- tableau2dtest/tableau2dtest/Program.cs
+++ tableau2dtest/tableau2dtest/Program.cs
@@ -6,61 +6,26 @@ namespace Morpion
 {
     class Program
     {
-    	 // Déclaration d'un tableau en 2 dimensions
-            // On définit la longueur de chaque dimensions
-            int [,] grilleInt = new int[3, 3];
+        public static int[,] grille = new int[3, 3]; // matrice pour stocker les coups joués
+
+        // Fonction permettant l'affichage du Morpion
+        public static void AfficherMorpion(int j, int k)
+        {
             string[,] grilleString = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
 
 
-            int[,] grilleInt2 = new int[6, 4];
-
-            // Pour accéder à la longueur du tableau dans sa dimensions 
-            // 0 pour la première dimensions
-            Console.WriteLine("Longeur première dimension : " + grilleInt.GetLength(0));
-            // 1 pour la seconde dimensions
-            Console.WriteLine("Longeur deuxième dimension: " + grilleInt.GetLength(1));
-            Console.WriteLine("Total : " + grilleInt.Length);
-            Console.WriteLine();
-            // Pour accéder à la longueur du tableau dans sa dimensions 
-            // 0 pour la première dimensions
-            Console.WriteLine("Longeur première dimension : " + grilleString.GetLength(0));
-            // 1 pour la seconde dimensions
-            Console.WriteLine("Longeur deuxième dimension: " + grilleString.GetLength(1));
-
-            // Parcourir un tableau int sans valeurs
-            for (var j = 0; j < grilleInt.GetLength(0); j++)
+            int[,] grilleInt2 = new int[3, 3];
+            for (var a = 0; a < grille.GetLength(0); a++)
             {
-                for (var i = 0; i < grilleInt.GetLength(1); i++)
-                {
-                    Console.WriteLine("Matrix : " + j.ToString() + " . " + i.ToString() + " valeur : " + grilleInt[j, i]);
-
-                }
             }
 
-            Console.WriteLine();
-
-            // Parcourir un tableau string avec des valeurs
-            for (var j = 0; j < grilleString.GetLength(0); j++)
+            for (var a = 0; a < grilleString.GetLength(0); a++)
             {
-                for (var i = 0; i < grilleString.GetLength(1); i++)
-                {
-                    Console.WriteLine("Matrix : " + j.ToString() + " . " + i.ToString() + " valeur : " + grilleString[j, i]);
-
-                }
             }
 
-            Console.WriteLine();
-            // Pour accéder à la longueur du tableau dans sa dimensions 
-            // 0 pour la première dimensions
-            Console.WriteLine("Longeur première dimension : " + grilleInt2.GetLength(0));
-            // 1 pour la seconde dimensions
-            Console.WriteLine("Longeur deuxième dimension: " + grilleInt2.GetLength(1));
-            Console.WriteLine("Affichage grille");
-
-            // Dessiner une grille
-            for (var j = 0; j < grilleInt2.GetLength(0); j++)
+            for (var a = 0; a < grilleInt2.GetLength(0); a++)
             {
-                Console.Write("\n|====|====|====|====|\n");
+                Console.Write("\n|====|====|====|\n");
                 Console.Write("|");
                 for (var i = 0; i < grilleInt2.GetLength(1); i++)
                 {
@@ -69,16 +34,10 @@ namespace Morpion
                 }
                 
             }
-            Console.Write("\n|====|====|====|====|");
+            Console.Write("\n|====|====|====|");
 
 
             Console.ReadKey();
-        public static int[,] grille = new int[3, 3]; // matrice pour stocker les coups joués
-
-        // Fonction permettant l'affichage du Morpion
-        public static void AfficherMorpion(int j, int k)
-        {
-            // A compléter 
         }
 
         // Fonction permettant de changer
@@ -107,7 +66,6 @@ namespace Morpion
             //--- Déclarations et initialisations --
             int LigneDébut = Console.CursorTop;     // par rapport au sommet de la fenêtre
             int ColonneDébut = Console.CursorLeft; // par rapport au sommet de la fenêtre
-
             int essais = 0;    // compteur d'essais
 	        int joueur = 1 ;   // 1 pour la premier joueur, 2 pour le second
 	        int l, c = 0;      // numéro de ligne et de colonne
@@ -122,10 +80,14 @@ namespace Morpion
 			        grille[j,k] = 10;
 					while(!gagner && essais != 9)
 					{
+						AfficherMorpion(j, k);
 
 						// A compléter 
 						try
 						{
+							Console.WriteLine();
+							Console.WriteLine();
+							Console.WriteLine();
 							Console.WriteLine("Ligne   =    ");
 							Console.WriteLine("Colonne =    ");
 							// Peut changer en fonction de comment vous avez fait votre tableau.
